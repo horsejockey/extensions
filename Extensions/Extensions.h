4433d2ead6c790ba75f80af79c144065bf055ac1
Matt McArthur <matt@cotopaxi.com>
2017-02-13 17:24:37 -0700

    initial commit

---
+++ Extensions/Extensions.h
@@ -0,0 +1,19 @@
+//
+//  Extensions.h
+//  Extensions
+//
+//  Created by Matthew McArthur on 2/13/17.
+//  Copyright © 2017 McArthur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Extensions.
+FOUNDATION_EXPORT double ExtensionsVersionNumber;
+
+//! Project version string for Extensions.
+FOUNDATION_EXPORT const unsigned char ExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Extensions/PublicHeader.h>
+
+
